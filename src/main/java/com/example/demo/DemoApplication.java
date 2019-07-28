f49cd2e898f8c069ee27356b3e8a35622dbec1c0
zhuolinho <zhuolinho@hotmail.com>
2019-07-28 12:01:04 +0800

    jdbc

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,63 +1,45 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
+@SpringBootApplication
 @Slf4j
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+    @Autowired
+    private FooDao fooDao;
+    @Autowired
+    private BatchFooDao batchFooDao;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @Bean
-    @ConfigurationProperties("foo.datasource")
-    public DataSourceProperties fooDataSourceProperties() {
-        return new DataSourceProperties();
+    @Autowired
+    public SimpleJdbcInsert simpleJdbcInsert(JdbcTemplate jdbcTemplate) {
+        return new SimpleJdbcInsert(jdbcTemplate).withTableName("FOO").usingGeneratedKeyColumns("ID");
     }
 
     @Bean
-    public DataSource fooDataSource() {
-        DataSourceProperties dataSourceProperties = fooDataSourceProperties();
-        log.info("foo datasource: {}", dataSourceProperties.getUrl());
-        return dataSourceProperties.initializeDataSourceBuilder().build();
+    @Autowired
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
     }
 
-    @Bean
-    @Resource
-    public PlatformTransactionManager fooTxManager(DataSource fooDataSource) {
-        return new DataSourceTransactionManager(fooDataSource);
-    }
-
-    @Bean
-    @ConfigurationProperties("bar.datasource")
-    public DataSourceProperties barDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    @Bean
-    public DataSource barDataSource() {
-        DataSourceProperties dataSourceProperties = barDataSourceProperties();
-        log.info("bar datasource: {}", dataSourceProperties.getUrl());
-        return dataSourceProperties.initializeDataSourceBuilder().build();
-    }
-
-    @Bean
-    @Resource
-    public PlatformTransactionManager barTxManager(DataSource barDataSource) {
-        return new DataSourceTransactionManager(barDataSource);
+    @Override
+    public void run(String... args) throws Exception {
+        fooDao.insertData();
+        batchFooDao.batchInsert();
+        fooDao.listData();
     }
 }
