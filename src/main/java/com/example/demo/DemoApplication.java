a216238b1ef0cb75aed613ee8483178d87699b4e
zhuolinho <zhuolinho@hotmail.com>
2019-08-04 21:32:40 +0800

    mongo repository

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import com.example.demo.converter.MoneyReadConverter;
 import com.example.demo.model.Coffee;
-import com.mongodb.client.result.UpdateResult;
+import com.example.demo.repository.CoffeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
@@ -12,24 +12,20 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-import static org.springframework.data.mongodb.core.query.Query.query;
 
 @SpringBootApplication
 @Slf4j
+@EnableMongoRepositories
 public class DemoApplication implements ApplicationRunner {
     @Autowired
-    private MongoTemplate mongoTemplate;
+    private CoffeeRepository coffeeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -43,20 +39,17 @@ public class DemoApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Coffee espresso = Coffee.builder().name("espresso").price(Money.of(CurrencyUnit.of("CNY"), 20.0)).createTime(new Date()).updateTime(new Date()).build();
-        Coffee saved = mongoTemplate.save(espresso);
-        log.info("Coffee {}", saved);
-
-        List<Coffee> list = mongoTemplate.find(Query.query(Criteria.where("name").is("espresso")), Coffee.class);
-        log.info("Find {} Coffee", list.size());
-        list.forEach(c -> log.info("Coffee {}", c));
+        Coffee latte = Coffee.builder().name("latte").price(Money.of(CurrencyUnit.of("CNY"), 30.0)).createTime(new Date()).updateTime(new Date()).build();
+        coffeeRepository.insert(Arrays.asList(espresso, latte));
+        coffeeRepository.findAll(Sort.by("name")).forEach(c -> log.info("Saved Coffee {}", c));
 
         Thread.sleep(1000);
-        UpdateResult result = mongoTemplate.updateFirst(query(where("name").is("espresso")), new Update().set("price", Money.ofMajor(CurrencyUnit.of("CNY"), 30)).currentDate("updateTime"), Coffee.class);
-        log.info("Update Result: {}", result.getModifiedCount());
-        Coffee updateOne = mongoTemplate.findById(saved.getId(), Coffee.class);
-        log.info("Update Result: {}", updateOne);
+        latte.setPrice(Money.of(CurrencyUnit.of("CNY"), 35.0));
+        latte.setUpdateTime(new Date());
+        coffeeRepository.save(latte);
+        coffeeRepository.findByName("latte").forEach(c -> log.info("Coffee {}", c));
 
-        mongoTemplate.remove(updateOne);
+        coffeeRepository.deleteAll();
     }
 
 
